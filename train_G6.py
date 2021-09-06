005000480d41bed4d9f057deaf17084a56064fd0
Carlos Dip <cadudip@hotmail.com>
2021-09-06 11:08:48 -0300

    Modified train protocol. Now automatically generates new images when original is inadequate.

--- train_G6.py
+++ train_G6.py
@@ -6,15 +6,123 @@ from IPython import get_ipython
 # %%
 from pathlib import Path
 import os
-Path("./model.pickle").touch()
+import pickle
+from G6_iris_recognition.feature_vec import engroup
+import cv2 as cv
+import matplotlib.pyplot as plt
 
-import G6_iris_recognition
-# G6_iris_recognition.iris_model_train("subset","model.pickle")
-# for label, image in zip(range(6), range(1,20)):
-#     print(f"./subset/{label:04d}/{label:04d}_{image:03d}.bmp")
-#     iris_name = G6_iris_recognition.iris_model_test("model.pickle",f"./subset/{label:04d}/{label:04d}_{image:03d}.bmp")
-#     print(f"The correct label is {label:04d}, the model predicted {iris_name}")
-labels=['0027', '0037', '0038', '0036', '0053', '0001', '0019', '0049', '0022']
-for i in range(9):
-    iris_name = G6_iris_recognition.iris_model_test("model.pickle",f"subset/0000/0000_000.bmp")
-    print(f"The correct label is {labels[i]}, the model predicted {iris_name}")
+def save_model(encodings, names):
+    # encodings = lista de listas de encondings gerados com engroup
+    # names = nome referente a cada lista da matriz encodings
+    data = {"encodings": encodings, "names": names}
+    with open("model.pickle", "wb") as f:
+        f.write(pickle.dumps(data))
+
+def cleanup_wrapper(path_to_img, path_to_tmp_file, transform_function, **kwargs):
+    # Wrapper for other functions, receives 'transform function' pointer
+    # 'transform function' must have following signature: tf(path_to_img, output_path, **kwargs)
+    if transform_function(path_to_img, path_to_tmp_file, **kwargs):
+        # print(f"Transformation was successfull")
+        return True
+    else:
+        return False
+
+def CLAHE_cleanup(path_to_image, path_to_save_output, cL = 30, tGS = (5,5)):
+    success = True
+    try:
+        # Read
+        img = cv.imread(path_to_image, cv.IMREAD_GRAYSCALE)
+        # Transform
+        processed_img = cv.medianBlur(img, 3)
+        clahe = cv.createCLAHE(clipLimit = cL, tileGridSize= tGS)
+        processed_img = clahe.apply(processed_img)
+        # Write
+        cv.imwrite(path_to_save_output, processed_img)
+    except:
+        # Handle failure
+        print(f"")
+        success=False
+    finally:
+        # Return state
+        return success
+
+def generic_cleanup(path_to_image, path_to_save_output):
+    success = True
+    try:
+        # Read
+        img = cv.imread(path_to_image, cv.IMREAD_GRAYSCALE)
+        # Transform
+        processed_img = img.copy() # Example, useless
+        # Write
+        cv.imwrite(path_to_save_output, processed_img)
+    except:
+        # Handle failure
+        print(f"")
+        success=False
+    finally:
+        # Return state
+        return success
+
+def main():
+    names = []
+    all_encodings = []
+    cleanup_options = [CLAHE_cleanup]
+    path_to_tmp_file = "tmp_img.bmp"
+    for folder in os.listdir("train"):
+        print(f"----\nStarting directory {folder}\n----\n")
+        current_encodings = []
+        any_successes_in_folder = False
+        for file in os.listdir(f"train/{folder}"):
+            processed=False
+            ## Tentando imagem original, e se não der, tentar imagem processada
+            path_to_image=f"train/{folder}/{file}"
+            iris_encodings_in_image = engroup(path_to_image)
+            if iris_encodings_in_image == "invalid image":
+                # Tentar todas as opções
+                curr_option_index = 0
+                while curr_option_index<len(cleanup_options) and not processed:
+                    # Select and advance option
+                    option=cleanup_options[curr_option_index]
+                    curr_option_index += 1
+                    # Try option
+                    if cleanup_wrapper(path_to_image, path_to_tmp_file, option):
+                        iris_encodings_in_image = engroup(path_to_tmp_file)
+                        if iris_encodings_in_image != "invalid image":
+                            processed = True
+                            print(f"[SUCCESS] Sucessfully processed {file}")
+                            any_successes_in_folder = True # Alguma íris dessa pasta deu certo então
+                            current_encodings.append(iris_encodings_in_image)
+            else:
+                current_encodings.append(iris_encodings_in_image)                            
+            if not processed:
+                print(f"[FAIL] Unable to process {file}")
+
+        print(f"Folder {folder} finished...")
+        if any_successes_in_folder:
+            print(f"Apending data...\n{len(current_encodings)} encodings extracted from this folder")
+            # Guardar nome da pasta como label e lista de encodings como features.
+            names.append(folder)
+            all_encodings.append(current_encodings)
+        else:
+            print("Unable to extract any information from folder...")
+    
+    save_model(all_encodings, names)
+
+
+if __name__ == "__main__":
+    Path("./model.pickle").touch()
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
