5f09f3ce36a3b513acd4eedde67342cef21cd294
Carlos Dip <cadudip@hotmail.com>
2021-09-18 12:37:47 -0300

    Parallelized tests and improved test logging.

---
+++ support_functions.py
@@ -0,0 +1,88 @@
+import numpy as np
+import cv2 as cv
+
+
+def cleanup_wrapper(path_to_img, path_to_tmp_file, transform_function, **kwargs):
+    # Wrapper for other functions, receives 'transform function' pointer
+    # 'transform function' must have following signature: tf(path_to_img, output_path, **kwargs)
+    if transform_function(path_to_img, path_to_tmp_file, **kwargs):
+        # print(f"Transformation was successfull")
+        return True
+    else:
+        return False
+
+def CLAHE_cleanup(path_to_image, path_to_save_output, cL = 30, tGS = (5,5)):
+    success = True
+    try:
+        # Read
+        img = cv.imread(path_to_image, cv.IMREAD_GRAYSCALE)
+        # Transform
+        imgMedian = np.median(img.flatten())
+        processed_img = cv.add(img, 127-imgMedian)
+        processed_img = cv.medianBlur(processed_img, 3)
+        clahe = cv.createCLAHE(clipLimit = cL, tileGridSize= tGS)
+        processed_img = clahe.apply(processed_img)
+        # Write
+        cv.imwrite(path_to_save_output, processed_img)
+    except:
+        # Handle failure
+        print(f"ERROR")
+        success=False
+    finally:
+        # Return state
+        return success
+
+def blurMorph_cleanup(path_to_image, path_to_save_output, kernel = np.ones((7,7),np.uint8), gaussBlurSize=(3,3)):
+    success = True
+    try:
+        # Read
+        img = cv.imread(path_to_image, cv.IMREAD_GRAYSCALE)
+        # Transform
+        imgMedian = np.median(img.flatten())
+        processed_img = cv.add(img, 127-imgMedian)
+        processed_img = cv.GaussianBlur(processed_img,gaussBlurSize,cv.BORDER_DEFAULT)
+        processed_img = cv.morphologyEx(processed_img, cv.MORPH_OPEN, kernel)
+        # Write
+        cv.imwrite(path_to_save_output, processed_img)
+    except:
+        # Handle failure
+        print(f"ERROR")
+        success=False
+    finally:
+        # Return state
+        return success
+
+def medianSlide_cleanup(path_to_image, path_to_save_output):
+    success = True
+    try:
+        # Read
+        img = cv.imread(path_to_image, cv.IMREAD_GRAYSCALE)
+        # Transform
+        imgMedian = np.median(img.flatten())
+        processed_img = cv.add(img, 127-imgMedian)
+        # Write
+        cv.imwrite(path_to_save_output, processed_img)
+    except:
+        # Handle failure
+        print(f"ERROR")
+        success=False
+    finally:
+        # Return state
+        return success
+
+def morphClose_cleanup(path_to_image, path_to_save_output, kernel = np.ones((5,5),np.uint8)):
+    success = True
+    try:
+        # Read
+        img = cv.imread(path_to_image, cv.IMREAD_GRAYSCALE)
+        # Transform
+        processed_img = cv.morphologyEx(img,cv.MORPH_CLOSE,kernel, iterations = 1)
+        # Write
+        cv.imwrite(path_to_save_output, processed_img)
+    except:
+        # Handle failure
+        print(f"ERROR")
+        success=False
+    finally:
+        # Return state
+        return success
--- test_G6.py
+++ test_G6.py
@@ -1,41 +1,107 @@
-from G6_iris_recognition import iris_model_test, iris_model_train
+from G6_iris_recognition import iris_model_test
+from pathlib import Path
 import os
+from G6_iris_recognition.feature_vec import engroup
+import multiprocessing as mp
+import time
+from support_functions import *
+# import tkinter.filedialog as tkfd
+import json
 import pickle
-from train_G6_parallel import blurMorph_cleanup, morphClose_cleanup, CLAHE_cleanup, medianSlide_cleanup, cleanup_wrapper
 
-
-
-miss = 0
-hit = 0 
-path_to_tmp_file = "tmp_img.bmp"
-# cleanup_options = [medianSlide_cleanup, CLAHE_cleanup, blurMorph_cleanup, morphClose_cleanup]
-cleanup_options = []
-curr_option_index = "Original"
-for folder in os.listdir(f"test/"):
-    current_encodings = []
+def process_folder(folder, cleanup_options, path_to_tmp_file, pickle_path):  
+    ## Processing
+    print(f"----\nStarting directory {folder}\n----\n")
+    predictions = []
     for file in os.listdir(f"test/{folder}"):
-        path_to_image=f"test/{folder}/{file}"
-        result = iris_model_test("model.pickle", path_to_image)
-        # if result == "unmatch":
-        #     # Tentar todas as opções
-        #     curr_option_index = 0
-        #     while curr_option_index<len(cleanup_options):
-        #         option=cleanup_options[curr_option_index]
-        #         curr_option_index += 1
-        #         if cleanup_wrapper(path_to_image, path_to_tmp_file, option):
-        #                     result = iris_model_test("./model.pickle", path_to_tmp_file)
-        #                     if result != "unmatch":
-        #                         break
-                         
-        print(f"img:{file}, result:{result}, folder {folder}, method : {curr_option_index}")
-        curr_option_index = "Original"
-        if result == folder:
-            hit +=1
+        processed=False
+        ## Tentando imagem original, e se não der, tentar imagem processada
+        path_to_image=f"test/{folder}/{file}"   
+        prediction = iris_model_test(pickle_path, path_to_image)
+        if prediction == "unmatch":
+            # Tentar todas as opções
+            curr_option_index = 0
+            while curr_option_index<len(cleanup_options) and not processed:
+                # Select and advance option
+                option=cleanup_options[curr_option_index]
+                curr_option_index += 1
+                # Try option
+                if cleanup_wrapper(path_to_image, path_to_tmp_file, option):
+                    prediction = iris_model_test(pickle_path, path_to_tmp_file)
+                    if prediction != "unmatch":
+                        processed = True
+                        # print(f"[SUCCESS] Sucessfully processed {file} with method {option.__name__}")
+                         # Alguma íris dessa pasta deu certo então
         else:
-            miss +=1
-print(f"HIT:{hit}")
-print(f"MISS:{miss}")
-print(f"TOTAL:{hit/(hit+miss)}")
+            processed = True
+             # Alguma íris dessa pasta deu certo então
+            # print(f"[SUCCESS] Sucessfully processed {file} with standard image")                              
+        # if not processed:
+            # print(f"[FAIL] Unable to process {file}")
+        # Saves last prediction made. If any prediction wasn't unmatch, saves that, otherwise, saves unmatch
+        predictions.append(prediction)
+    print(f"Folder {folder} finished...")
+    try:
+        os.remove(path_to_tmp_file)
+    finally:
+        return {f"{folder}":predictions}
+
+def main(processes=os.cpu_count(), cleanup_options = [], debug = False, pickle_path="model.pickle"):
+    
+    # File dialog for selecting model to test
+    # pickle_path = ""
+    # pickle_path = tkfd.askopenfilename(title='Indicate model.pickle location')
+    # while not pickle_path.endswith(".pickle"):
+    #     pickle_path = tkfd.askopenfilename(title='INVALID: Indicate model.pickle location')
+    
+    try:
+        pool = mp.Pool(processes)
+        if debug:
+            multiple_results = [pool.apply_async(process_folder, (folder, cleanup_options, f"tmp_img{i}.bmp", pickle_path)) for i, folder in enumerate(os.listdir("test")[:10])]
+        else:
+            multiple_results = [pool.apply_async(process_folder, (folder, cleanup_options, f"tmp_img{i}.bmp", pickle_path)) for i, folder in enumerate(os.listdir("test"))]
+        output = [res.get() for res in multiple_results]        
 
 
+        summary=f"{'='*8}\n SUMMARY \n{'='*8}\n"
+        data = pickle.loads(open(pickle_path, "rb").read())        
+        summary+=f"Known categories: { data['names'] }\n\n"
+        hit, count = 0, 0
+        for p in output:
+            folder = list(p.keys())[0]
+            values = list(p.values())[0]
+            for v in values:
+                if v == folder:
+                    hit += 1
+                count += 1
+            summary+=f"{folder}: {values}\n"
+        summary += f"Hits: {hit}/{count}, Misses: {count-hit}/{count}, Score: {(hit/count):.03f}\n"
+
+        with open("test_result.txt","w") as f:
+            f.write(summary)
+        
+
+    finally:
+        pool.terminate()
+        pool.join()
+
+if __name__ == "__main__":
+    ##=============================================##
+    ##=====DO NOT USE CTRL+C TO STOP PROCESS=======##
+    ##=======THIS WILL CRASH YOUR TERMINAL=========##
+    ##=======IT IS A KNOWN BUG IN mp.Pool==========##
+    ##========bugs.python.org/issue38428===========##
+    ##=============================================##
+
+
+    # Necessary options. Indicate pickle model location, and add the functions used to train the model to the list below.
+    cleanup_options = []#[blurMorph_cleanup, morphClose_cleanup, CLAHE_cleanup, medianSlide_cleanup]
+    pickle_path = "model_version_0/model_no_mod.pickle"
+
+    print(f"Begining processing")
+    start = time.perf_counter()
+    cleanup_options = []
+    main(processes=7, cleanup_options=cleanup_options, pickle_path=pickle_path)
+    end = time.perf_counter()-start
+    print(f"Finished in {end//60} minutes and {end%60} seconds")
 
